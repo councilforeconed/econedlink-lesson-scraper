60cbffcbb9e0a9730d7e66a9f527566d5dafe207
Steve Kinney <hello@stevekinney.net>
2013-08-30 15:03:37 -0400

    Prefix the lesson IDs

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ var ActiveLessonStream = require('./lib/streams/active-lesson-stream.js');
 var LessonInfoStream = require('./lib/streams/lesson-info-stream.js');
 var ApplyEconomicsStandards = require('./lib/streams/apply-econ-standards-stream.js');
 var ApplyPersonalFinanceStandards = require('./lib/streams/apply-personal-finance-standards-stream.js');
+var ApplyPrefixToLessonIDStream = require('./lib/streams/append-prefix-stream.js');
 
 ActiveLessonStream()
   .pipe(LessonInfoStream())
   .pipe(ApplyEconomicsStandards())
   .pipe(ApplyPersonalFinanceStandards())
+  .pipe(ApplyPrefixToLessonIDStream())
   .on('data', function (data) {
     console.log(data);
   });
